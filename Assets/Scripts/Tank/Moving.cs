6963fefe2e9c6c608e60cb635f2930bdefe58f69
Gabriele <154472848+CetriGabrio@users.noreply.github.com>
2026-02-09 11:18:01 -0500

    Slider for speed is working

--- Assets/Scripts/Tank/Moving.cs
+++ Assets/Scripts/Tank/Moving.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Moving : MonoBehaviour
 {
     public float speed = 1f;
+
+    public Slider slider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        slider.minValue = 0;
+        slider.maxValue = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
+        speed = slider.value;
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
@@ -22,5 +28,10 @@ public class Moving : MonoBehaviour
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
+    }
+
+    public void setSpeed(float value)
+    {
+        speed = value;
     }
 }
